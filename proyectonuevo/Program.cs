8b0ea20ad0489d3a1c43b0d3800a6d6041ceabf0
karmilo900 <camilopaduamayordomo@gmail.com>
2024-03-31 13:15:40 -0500

    registro de datos y hora de entrada y salida del empleado desde consola

--- proyectonuevo/Program.cs
+++ proyectonuevo/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using empleadocontroller;
-using MySql.Data.MySqlClient;
 
 class Program
 {
     static void Main(string[] args)
     {
-        DateTime hora = new DateTime(1, 1, 1, 18, 30, 0);
-        DateTime horasal = new DateTime(1, 1, 1, 23, 30, 0);
+        int ident = 0;
+        string nombre = "";
+        DateTime horain, horasal;
+
+        Console.WriteLine("Por favor escriba el # de identificación del empleado: ");
+        ident = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Por favor escriba el nombre del empleado: ");
+        nombre = Console.ReadLine();
+
+        Console.WriteLine("Por favor escriba la hora de ingreso del empleado (en formato HH:mm:ss): ");
+        if (!DateTime.TryParseExact(Console.ReadLine(), "HH:mm:ss", null, System.Globalization.DateTimeStyles.None, out horain))
+        {
+            Console.WriteLine("Formato de hora incorrecto. Por favor, introduzca la hora en el formato correcto (HH:mm:ss).");
+            return;
+        }
+
+        Console.WriteLine("Por favor escriba la hora de salida del empleado (en formato HH:mm:ss): ");
+        if (!DateTime.TryParseExact(Console.ReadLine(), "HH:mm:ss", null, System.Globalization.DateTimeStyles.None, out horasal))
+        {
+            Console.WriteLine("Formato de hora incorrecto. Por favor, introduzca la hora en el formato correcto (HH:mm:ss).");
+            return;
+        }
+
         EmpleadoController empControl = new EmpleadoController();
-        empControl.registrarEmpleado(103423223,"juan alberto",hora,horasal);
-        
+        empControl.registrarEmpleado(ident, nombre, horain, horasal);
     }
 }
